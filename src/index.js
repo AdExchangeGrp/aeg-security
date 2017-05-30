b53ef912fe7d9c2b85d8cf2f3037a70ca699b34b
Justin Durham <justin.durham@icloud.com>
2017-05-30 12:21:32 -0400

    Cleanup security

--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
-import DB from './db';
-import Account from './account';
-import ApiKey from './api-key';
-import Application from './application';
-import Directory from './directory';
-import Group from './group';
-import Organization from './organization';
 import Token from './token';
 
 export default {
-	DB,
-	Account,
-	ApiKey,
-	Application,
-	Directory,
-	Group,
-	Organization,
 	Token
 };
 
 export {
-	DB,
-	Account,
-	ApiKey,
-	Application,
-	Directory,
-	Group,
-	Organization,
 	Token
 };
